bf51652bc1f60881391bd16f45d47db253e3959a
dacchan <0x0187d350@gmail.com>
2019-02-10 17:50:32 +0900

    [Enhancement][RawRepresentable] Add transform method `map()`.

---
+++ Tests/Standard/RawRepresentable.swift
@@ -0,0 +1,32 @@
+//
+//  RawRepresentable.swift
+//  Swiftensions-iOSTests
+//
+//  Created by dacchan on 2019/02/10.
+//  Copyright © 2019年 dacchan. All rights reserved.
+//
+
+import XCTest
+
+import Swiftensions
+
+class RawRepresentable: XCTestCase {
+
+    func testMap() {
+        enum Animal: String {
+            case cat, tiger, lion
+        }
+
+        let animal: Animal = .cat
+
+        let face = animal.map { a -> String in
+            switch a {
+            case .cat: return "🐱"
+            case .tiger: return "🐯"
+            case .lion: return "🦁"
+            }
+        }
+
+        XCTAssert(face == "🐱")
+    }
+}
