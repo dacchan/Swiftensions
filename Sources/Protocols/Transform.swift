011aaac856e4d36aa036c0eb3e125d9b85d9b497
dacchan <0x0187d350@gmail.com>
2019-02-10 17:50:59 +0900

    [Enhancement][TransformProtocol] Add `TransformProtocol`.

---
+++ Sources/Protocols/Transform.swift
@@ -0,0 +1,17 @@
+//
+//  Transform.swift
+//  Swiftensions-iOS
+//
+//  Created by dacchan on 2019/02/10.
+//  Copyright © 2019年 dacchan. All rights reserved.
+//
+
+public protocol TransformProtocol {
+    func map<U>(_ f:(Self) -> U) -> U
+}
+
+public extension TransformProtocol {
+    func map<U>(_ f:(Self) -> U) -> U {
+        return f(self)
+    }
+}
